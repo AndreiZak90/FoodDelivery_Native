6920b3fdf27d27c8a40523dae4fa63ec75bc7557
AndreiZak <zak-90@inbox.ru>
2025-06-21 18:19:31 +0300

    первичная настройка

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { grey, orange, white, yellow } from "@/constants/Colors";
+import { Text, useColorScheme, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      <SafeAreaView style={(styles.container, themeContainerStyle)}>
+        <Text style={[styles.text, themeTextStyle]}>
+          Color scheme: {colorScheme}
+        </Text>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 200,
+  },
+  lightContainer: {
+    backgroundColor: yellow,
+  },
+  darkContainer: {
+    backgroundColor: orange,
+  },
+  lightThemeText: {
+    color: white,
+  },
+  darkThemeText: {
+    color: grey,
+  },
+});
---
+++ constants/Colors.ts
@@ -0,0 +1,7 @@
+export const allWhite = "#ffffff";
+export const white = "#f9f9f9";
+export const background = "#f2f2f2";
+export const black = "#000000";
+export const orange = "#ff7020";
+export const yellow = "#ffab08";
+export const grey = "#b1b1b1";
