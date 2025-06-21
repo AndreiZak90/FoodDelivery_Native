8b442713a0885ee0afcb889a1788d3bb6ae1d8da
AndreiZak <zak-90@inbox.ru>
2025-06-21 17:22:00 +0300

    First commit

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false, //убрали видимость заголовков
+      }}
+    >
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
